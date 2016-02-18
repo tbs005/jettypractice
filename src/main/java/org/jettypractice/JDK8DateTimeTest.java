93d4cd3a2c9368f12f19df4aa6936f755cf12e6e
tbs005 <tbs005@qq.com>
2016-02-18 15:50:25 +0800

    添加joda-time日期工具类 测试JDK8新增的日期工具类

---
+++ src/main/java/org/jettypractice/JDK8DateTimeTest.java
@@ -0,0 +1,46 @@
+package org.jettypractice;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class JDK8DateTimeTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Arrays.asList("0.0.0.0/0", "aaa"));
+
+		// Get the system clock as UTC offset
+		final Clock clock = Clock.systemUTC();
+		System.out.println(clock.instant());
+		System.out.println(clock.millis());
+
+		// Get the local date and local time
+		final LocalDate date = LocalDate.now();
+		final LocalDate dateFromClock = LocalDate.now(clock);
+
+		System.out.println(date);
+		System.out.println(dateFromClock);
+
+		// Get the local date and local time
+		final LocalTime time = LocalTime.now();
+		final LocalTime timeFromClock = LocalTime.now(clock);
+
+		System.out.println(time);
+		System.out.println(timeFromClock);
+
+		// Get the local date/time
+		final LocalDateTime datetime = LocalDateTime.now();
+		final LocalDateTime datetimeFromClock = LocalDateTime.now(clock);
+
+		System.out.println(datetime);
+		System.out.println(datetimeFromClock);
+	}
+}
