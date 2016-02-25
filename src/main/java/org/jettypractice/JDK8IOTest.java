56b6cfb61be4689176754d7eadbf655041e74755
tbs005 <tbs005@qq.com>
2016-02-25 11:05:52 +0800

    Java IO 测试类

---
+++ src/main/java/org/jettypractice/JDK8IOTest.java
@@ -0,0 +1,36 @@
+package org.jettypractice;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * <p>
+ * Title:JavaIO 测试类
+ * </p>
+ * <p>
+ * Description:Java IO API
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
+ * @author tbs005 tbs005@qq.com
+ * @date 2016年2月25日上午10:49:23
+ */
+public class JDK8IOTest {
+	public static void main(String[] args) throws IOException {
+		File file = new File("d:\\javaio.txt");
+		System.out.println(file.exists());
+		System.out.println(file.isDirectory());
+		System.out.println(file.getAbsolutePath());
+		System.out.println(file.getPath());
+		
+		
+		FileInputStream in = new FileInputStream(file);
+		byte[] bytes = new byte[1024];
+		in.read(bytes);
+		in.close();
+
+	}
+}
