c566e360c1c791ff7c5d3aa415fe3106580ef1f7
tbs005 <tbs005@qq.com>
2016-02-25 11:07:31 +0800

    Java NIO测试类

---
+++ src/main/java/org/jettypractice/JDK8NIOTest.java
@@ -0,0 +1,20 @@
+/**
+ * @tile:JDK8NIOTest.java
+ * @package:org.jettypractice
+ * @Description:TODO
+ * @author tbs005 tbs005@qq.com
+ * @date 2016年2月25日上午11:06:27
+ * @version v1.0
+ */
+package org.jettypractice;
+
+/**
+ * <p>Title:JDK8 Java NIO Test</p>
+ * <p>Description:此类用来测试Java NIO 相关API</p>
+ * <p>Company:</p>
+ * @author tbs005 tbs005@qq.com
+ * @date 2016年2月25日上午11:06:27
+ */
+public class JDK8NIOTest {
+
+}
