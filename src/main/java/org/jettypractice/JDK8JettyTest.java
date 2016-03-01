9386431be928f6685b606cb328da4deffd2e23a2
tbs005 <tbs005@qq.com>
2016-03-01 13:52:30 +0800

    jetty 嵌入式测试代码

---
+++ src/main/java/org/jettypractice/JDK8JettyTest.java
@@ -0,0 +1,24 @@
+/**
+ * @tile:JDK8JettyTest.java
+ * @package:org.jettypractice
+ * @Description:TODO
+ * @author tbs005 tbs005@qq.com
+ * @date 2016年3月1日下午1:50:48
+ * @version v1.0
+ */
+package org.jettypractice;
+
+/**
+ * <p>Title:Jetty测试</p>
+ * <p>Description:使用嵌入式jetty来开发web应用</p>
+ * <p>Company:</p>
+ * @author tbs005 tbs005@qq.com
+ * @date 2016年3月1日下午1:50:48
+ */
+public class JDK8JettyTest {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
